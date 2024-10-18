d3e0c686774153707a57e896cfb81c20bec84d73
Albert <albertpencas@gmail.com>
2024-10-18 10:34:11 +0200

    canvis

--- codeJS/code.js
+++ codeJS/code.js
@@ -1,23 +1,5 @@
-//ex2
-//Fes un programa que demani un valor a l’usuari i indiqui si el valor és un número.
-//Fes servir l’operador ternari per mostrar la informació.*
+//ex3
+// Fes un programa que permeti realitzar la conversió entre temperatures de 
+// ºC a ºF  i de ºF a ºC, d’acord amb la següent fórmula: ºF = (9.0/5.0)* (ºC)+32. 
 
-let valor = prompt("Introdeuix un número: ")
-
-//Si valor == true, és a dir, si introdueix un valor:
-if (valor) {
-    //Convertim el valor a número (si pot)
-    const num = Number(valor);
-
-    //Variable que mostrarem per pantalla
-    let text = "";
-
-    //Comproba que no sigui un number --> isNaN (is Not a Number)
-    text = isNaN(num) ? "No és un número" : "És un número"
-    
-    //Imprimir per pantalla
-    alert(text)
-}
-else{
-    alert("Error, has d'introduïr mínim un valor.")
-}
+//El programa ha de demanar a l’usuari quina conversió ha de fer (“C” o “F”) i la temperatura que es vol convertir.
