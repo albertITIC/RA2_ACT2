ba8daa4181e6f258bc3e59f752a1a72d8147e249
Albert <albertpencas@gmail.com>
2024-10-22 10:44:08 +0200

    ex2 modificat

--- codeJS/code.js
+++ codeJS/code.js
@@ -1,23 +1,21 @@
 //ex2
-//Fes un programa que demani un valor a l’usuari i indiqui si el valor és un número.
-//Fes servir l’operador ternari per mostrar la informació.*
+// Fes un programa que demani un valor a l’usuari i indiqui si el valor
+// és un enter o un decimal.*
 
-let valor = prompt("Introdeuix un número: ")
+let num = prompt("Introdeuix un número: ")
 
-//Si valor == true, és a dir, si introdueix un valor:
-if (valor) {
-    //Convertim el valor a número (si pot)
-    const num = Number(valor);
+//S'ha de passar a número (si no ho agafa com a String)
+num = Number(num);
 
-    //Variable que mostrarem per pantalla
-    let text = "";
-
-    //Comproba que no sigui un number --> isNaN (is Not a Number)
-    text = isNaN(num) ? "No és un número" : "És un número"
-    
-    //Imprimir per pantalla
-    alert(text)
+//Comprovar si el número és un número
+if (!isNaN(num)) {
+    if(Number.isInteger(num)){
+        console.log("El número introduït és un enter")
+    }
+    else{
+        console.log("És un decimal")
+    }
 }
 else{
-    alert("Error, has d'introduïr mínim un valor.")
+    alert("Error, has d'introduïr un número vàlid.")
 }
