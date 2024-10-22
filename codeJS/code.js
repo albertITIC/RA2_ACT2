3a5899f0f068516e02ba929f2a878cd853ce5da8
Albert <albertpencas@gmail.com>
2024-10-22 12:57:34 +0200

    inici ex5

--- codeJS/code.js
+++ codeJS/code.js
@@ -1,38 +1,31 @@
-//ex4
-// Fes un programa que demani a l’usuari el temps total 
-// d’una trucada demanant primer els minuts i després els segons.
+//ex5
+//Fes un programa que demani a l’usuari un número de 5 dígits,
+//comprovi que s’ha afegit correctament els 5 dígits i informi 
+//si aquest número comença i acaba amb el mateix valor.
 
-// Mostreu l’import sense IVA i calculant l’IVA (21%) sabent que la 
-// companyia telefònica cobra 10 cèntims per establir una connexió telefònica i a
-// partir d’aquest moment, 5 cèntims per minut tarificant per segons.
+let num = prompt("Introdueix un número de 5 digits: ")
 
-//Demano al usuari els valors inicials
-let minuts = prompt("Introdueix els minuts total: ");
-let segons = prompt("Introdueix els segons total: ");
+if (num) {
+    if (num.length == 5) {
+        num = Number(num)
+        if (!isNaN(num)) {
 
-//Centims que s'afegeixen per quan s'inicia una trucada telefónica
-let iniciTrucada = 10;
 
-//Centims per minut
-let cMinut = 5;
 
-//Passem minunts i segons a number (int)
-minuts = Number(minuts)
-segons = Number(segons)
 
-//Calcular preu sense IVA
-//comprovem que hagi posat algun tipus de valor dins de minutos o segons
-if(minuts || segons){
-    calcularMinuts = iniciTrucada + (cMinut * minuts);
-    //calcular els segons
-    calculcarSegons = calcularMinuts + segons;
-    //Variable per guardar el total i aplicar-li després l'IVA
-    preuSenseIVA = calculcarSegons;
-    preuAmbIVA = preuSenseIVA*1.21;
+        }
+        else {
+            alert("ERROR. Has d'introduïr un número.")
+        }
+    }
+    else {
+        alert("ERROR. Has d'introduïr un número de 5 xifres.")
+    }
 
-    console.log("Total preu sense IVA: ", preuSenseIVA);
-    console.log("Total preu amb IVA: ", preuAmbIVA);
+    //Comprovo que sigui un número
+
+
 }
-else{
-    alert("Introdueix uns valors vàlids")
+else {
+    alert("ERROR. Introdueix un valor vàlid.")
 }
