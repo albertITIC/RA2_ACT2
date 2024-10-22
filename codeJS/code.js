9b6ddfb438fd92c6fa34cd5b78737301e8719aa2
Albert <albertpencas@gmail.com>
2024-10-22 13:17:45 +0200

    exercici 5 acabat

--- codeJS/code.js
+++ codeJS/code.js
@@ -6,13 +6,18 @@
 let num = prompt("Introdueix un número de 5 digits: ")
 
 if (num) {
-    if (num.length == 5) {
-        num = Number(num)
-        if (!isNaN(num)) {
+    if (num.length === 5) {
+        const numeroValid = Number(num)
+        
+        if (!isNaN(numeroValid)) {
+            console.log("La cadena de número s'ha passat a: ", typeof numeroValid)
 
-
-
-
+            if(num[0]===num[4]){
+                console.log("El primer número i l'últim coinicideixen")
+            }
+            else{
+                console.log("El primer número i l'últim no coinicideixen")
+            }
         }
         else {
             alert("ERROR. Has d'introduïr un número.")
